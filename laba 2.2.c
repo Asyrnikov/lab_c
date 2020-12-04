eea215ae24ed946892ad39a36f840747de66af49
Asyrnikov <71493974+Asyrnikov@users.noreply.github.com>
2020-12-04 11:13:18 +0300

    Update laba 2.2.c

--- laba 2.2.c
+++ laba 2.2.c
@@ -8,15 +8,15 @@ int main() {
 	scanf("%d %d %d", &a, &b, &c);
 	count = 0;
 	
-	if(a>0){	
+	if(a > 0){	
 	    count++;
 		}
 	
-	if(b>0){
+	if(b > 0){
 		count++;	
 	}
 	
-	if(c>0){
+	if(c > 0){
 		count++;
 	}
 	
